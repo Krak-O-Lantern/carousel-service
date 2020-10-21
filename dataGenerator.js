33e8f3f9cd3f398c94962c402f7f3dabb8d072bb
Eric Wise <ewise82@gmail.com>
2020-10-21 14:53:16 -0700

    modify dataGeneration file

--- dataGenerator.js
+++ dataGenerator.js
@@ -43,19 +43,13 @@ const startWriting = (writeStream, encoding, done) => {
     do {
       i -= 1;
       const post = createPost(i);
-      // check if i === 0 so we would write and call `done`
       if (i === 0) {
-        // we are done so fire callback
         writeStream.write(post, encoding, done);
       } else {
-        // we are not done so don't fire callback
         writeStream.write(post, encoding);
       }
-      // else call write and continue looping
     } while (i > 0 && canWrite);
     if (i > 0 && !canWrite) {
-      // our buffer for stream filled and need to wait for drain
-      // Write some more once it drains.
       writeStream.once('drain', writing);
     }
   }
